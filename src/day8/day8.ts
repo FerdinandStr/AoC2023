c356d64ea20d00b25442f076d5ae5e025187bd94
Ferdinand Straß <strass.fred@gmail.com>
2023-12-11 19:51:02 +0100

    day8 part 1

---
+++ src/day8/day8.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs"
+
+const direction =
+    "LRRLRRLRRRLRLLRRRLLRRRLRLRRRLRLRRLRRRLRRRLRLRRRLRRRLRRLRRRLLLRLRRRLRRRLRRRLRLRLRRLLRRRLRLLRLRRRLRRLLRLRLRRLRRRLRRLLRLRRRLLRRLRRRLRLRRLLRRRLRRLLRRLRRRLRLRRRLRRLRRRLRRRLRRLRRRLRLRRLRRRLRRRLRRLLRLRRLRRLRRRLRLLLRRRLLRRRLRLRRRLRLRRLRRRLLLRLRRRLRLRRLRRRLRRRLRRLRLRLRRRR"
+
+loopOverInput()
+
+function readInput() {
+    const input = readFileSync("./src/day8/input", "utf-8").split(/\n/)
+    return input.map((el) => {
+        const location = el.split(" = ")
+
+        return {
+            [location[0]]: location[1].replace("(", "").replace(")", "").split(", "),
+        }
+    })
+}
+
+function loopOverInput() {
+    const input = readInput()
+
+    let position = "AAA"
+    const end = "ZZZ"
+
+    let step = 0
+
+    while (position != end) {
+        const foundPos = input.find((el) => Object.keys(el)[0] == position)
+        console.log("POS", foundPos[position])
+        let next_direction = direction[step % direction.length] == "R" ? 1 : 0
+        console.log("next dir:", next_direction)
+        console.log("POS", foundPos[position])
+        position = foundPos[position][next_direction]
+
+        console.log("POSITION", position)
+        step += 1
+    }
+
+    console.log("steps:", step)
+
+    //SOLUTION 11309
+}
+
+function moveToNextDestination() {}
