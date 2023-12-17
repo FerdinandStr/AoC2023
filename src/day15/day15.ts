beec9c136ed63758e6f0a4e794ce03f71ffc83c0
Ferdinand Straß <strass.fred@gmail.com>
2023-12-17 21:23:45 +0100

    day15 finish

--- src/day15/day15.ts
+++ src/day15/day15.ts
@@ -1,9 +1,8 @@
 import { readFileSync } from "fs"
 
+// const input = "HASH"
 // const input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 const input = readFileSync("./src/day15/input", "utf-8").replaceAll("\n", "")
-
-hashInput(input)
 
 // Letters at start = Lens
 // hash of Letters = box
@@ -16,33 +15,93 @@ hashInput(input)
 // 2) if no match, add lens at back
 
 const expectedResult = [["rn=1", "cm=2"], [], [], ["ot=7", "ab=5", "pc=6"]]
+let boxArray: string[][] = []
+for (let i = 0; i < 256; i++) {
+    boxArray.push([])
+}
 
-let boxArray = new Array<string[]>(256)
+hashInput(input)
 
-putLensInBoxArray()
+function putLensInBoxArray(element: string) {
+    console.log(boxArray[1])
+    const indexOfOperator = getIndexOfOperator(element)
+    const lensLabel = element.slice(0, indexOfOperator)
+    const boxId = hashOfElement(lensLabel)
+    let box = boxArray[boxId]
+    console.log("START", element, "Box", boxId)
+    const operation = element.slice(indexOfOperator, indexOfOperator + 1)
 
-function putLensInBoxArray() {
-    console.log(boxArray)
+    const lensLabelIndexInBox = box.findIndex((el) => el.slice(0, indexOfOperator) == lensLabel)
+
+    console.log("IsInNBox=?", lensLabelIndexInBox)
+    if (operation == "=") {
+        if (lensLabelIndexInBox != -1) {
+            //REPLACE LENS WITH NEW LENS
+            box[lensLabelIndexInBox] = element
+        } else {
+            //ADD NEW LENS TO BACK IF NOT IN BOX
+            box.push(element)
+        }
+    } else {
+        //DELETE LENS ON "-" OPERATION
+        box = box.filter((el) => el.slice(0, indexOfOperator) != lensLabel)
+    }
+
+    //put updated box back in boxArray
+    boxArray[boxId] = box
+    // console.log(box)
+    // console.log(boxArray)
+}
+
+function hashOfElement(element: string) {
+    return element.split("").reduce((acc, char) => ((acc + hashCharacter(char)) * 17) % 256, 0)
 }
 
 function hashInput(input: string) {
     const inputArray = input.split(",")
     let globalSum = 0
 
-    for (const string of inputArray) {
-        // console.log(string)
-        let currentValue = 0
-        for (const char of string) {
-            currentValue = currentValue + hashCharacter(char)
-            // console.log("ASCII", currentValue)
-            currentValue = currentValue * 17
-            currentValue = currentValue % 256
-            // console.log(currentValue)
-        }
+    for (const element of inputArray) {
+        console.log(element)
+
+        putLensInBoxArray(element)
+
+        // console.log(hashOfElement(element))
+        let currentValue = hashOfElement(element)
+        // for (const char of element) {
+        //     currentValue = currentValue + hashCharacter(char)
+        //     // console.log("ASCII", currentValue)
+        //     currentValue = currentValue * 17
+        //     currentValue = currentValue % 256
+        //     // console.log(currentValue)
+        // }
         // console.log("RUN", currentValue)
         globalSum = globalSum + currentValue
     }
-    console.log(globalSum)
+
+    const totalFocusingPower = boxArray.reduce((acc, box) => {
+        let boxFocusingPower = 0
+        box.forEach((lens, lensSlot) => {
+            const indexOfOperator = getIndexOfOperator(lens)
+            const lensLabel = lens.slice(0, indexOfOperator)
+            const boxId = hashOfElement(lensLabel)
+            const focalLength = lens.slice(indexOfOperator + 1, indexOfOperator + 2)
+            const lensfocusingPower = (boxId + 1) * (lensSlot + 1) * Number(focalLength)
+            // console.log("INFO", lens, lensLabel, boxId, focalLength)
+            boxFocusingPower = boxFocusingPower + lensfocusingPower
+        })
+        return acc + boxFocusingPower
+    }, 0)
+
+    console.log(totalFocusingPower) //244199
+    console.log(globalSum) //514281
+}
+
+function getIndexOfOperator(element: string) {
+    console.log("INDEXCALC", element)
+    const operatorIndex = element.indexOf("=") != -1 ? element.indexOf("=") : element.indexOf("-")
+    console.log(operatorIndex)
+    return operatorIndex
 }
 
 function hashCharacter(char: string) {
